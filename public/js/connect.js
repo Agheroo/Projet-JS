44e76c29b73574d701d556262ce1b17537fa50fd
Renan <reauxrenan@outlook.fr>
2023-05-08 18:04:06 +0200

    added help for back

---
+++ public/js/connect.js
@@ -0,0 +1,17 @@
+const checkUserConnect = () =>{
+    let inputs = ["email","password"];
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
+    for(let input in inputs){
+        console.log(inputs[input]);
+        if(document.getElementById(inputs[input]).value == ""){
+            document.getElementById(inputs[input]).style.border = "solid 1px red";
+            alert("Veuillez rentrer des informations valides");
+            return;
+        }
+        else
+        document.getElementById(inputs[input]).style.border = "none";
+    }
+
+    //Then verify input informations with back to proceed to connexion
+}
---
+++ public/js/register.js
@@ -0,0 +1,25 @@
+const checkAddUser = () =>{
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
+    let confirm_pass = document.getElementById("repeat-password").value;
+    let inputs = ["username","email","password","repeat-password"];
+    for(let input in inputs){
+        console.log(inputs[input]);
+        if(document.getElementById(inputs[input]).value == ""){
+            document.getElementById(inputs[input]).style.border = "solid 1px red";
+            alert("Veuillez rentrer des informations valides");
+            return;
+        }
+        else
+        document.getElementById(inputs[input]).style.border = "none";
+    }
+    if(pass != confirm_pass){
+        document.getElementById("repeat-password").style.border = "solid 1px red";
+        alert("Veuillez vérifier votre mot de passe");
+        return;
+    }
+
+
+    //Then process the values in input to link it to back
+}
